c05952c48262069f9f9472c7a8ef88aacc4d23c5
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-19 14:30:59 +0200

    project complete
    Changed image array to an array with objects and then used the new array
    to feed the values to the DOM.
    Added html content and style to position imageI.title and
    image.description of each object inside #slider.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,50 @@
-const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
+const images = 
+[
+    {
+        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        title: 'Svezia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
 
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Perù',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+        title: 'Chile',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Argentina',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        title: 'Colombia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+];
 let activeImage = 0;
 
 const sliderDom = document.querySelector('.slider');
 const thumbnailsDom = document.querySelector('.thumbnails')
 
 for (let i = 0; i < images.length; i++) {
+    const imagesI = images[i]
     sliderDom.innerHTML += `<div class="card">
-                                <img class="card-img" src="${images[i]}">
+                                <div id="text-overlay">
+                                    <h2>${imagesI.title}</h2>
+                                    <p>${imagesI.description}</p>
+                                </div>
+                                <img class="card-img" src="${imagesI.url}">
                             </div>`;
     thumbnailsDom.innerHTML += `<div class="img-wrap">
                                     <div class="thumbnail">
-                                        <img class="thumbnail-img" src="${images[i]}">
+                                        <img class="thumbnail-img" src="${imagesI.url}">
                                         <div class="thumbnail-overlay"></div>
                                     </div>
                                 </div>`;
